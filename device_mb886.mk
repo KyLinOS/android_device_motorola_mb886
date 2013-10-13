40b12daabe602d31dcc9582d94a489f9bfa9dc1c
ZeroJim <zerojim520@gmail.com>
2013-10-13 20:51:36 +0800

    git commit -m "Merge branch 'jb-mr1' into kylin"

--- device_mb886.mk
+++ device_mb886.mk
@@ -31,7 +31,7 @@ endif
 # mb886 specific overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_LOCALES := en_US
+PRODUCT_LOCALES := zh_CN
 PRODUCT_LOCALES += xhdpi
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
